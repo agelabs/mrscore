ae2e02b8b59099d952e5184daf124584ed1a0083
Espen Riskedal <espen@cutehacks.com>
2018-06-21 14:23:43 +0200

    feat: calculate MRscore and contMRscore
    - MRscore is sum of CpG sites lower or higher than the cutoffs
    - contMRscore is sum of Multipliers*VALUE vectors
    
    Note: No normalization is performed

--- mrscore.R
+++ mrscore.R
@@ -1,2 +1,14 @@
+# Usage:
+#
+# source("mrscore.R")
+# calculateMRscore(Table(getGEO("GSM1886935")))
+
+
 MRscoreCpgSites = read.csv("cpgsites_multipliers_cutoffs.csv", strip.white = TRUE, blank.lines.skip = TRUE, comment.char = "#")
 
+calculateMRscore = function(geoMethylationTable) {
+  mergedTable = merge(MRscoreCpgSites, geoMethylationTable, by.x = "CpGsites", by.y = "ID_REF")
+  noncontMR = sum(mergedTable$VALUE <= mergedTable$Lower) + sum(mergedTable$VALUE >= mergedTable$Higher)
+  contMR = sum(mergedTable$VALUE * mergedTable$Multipliers)
+  return(list("MRscore" = noncontMR, "contMRscore" = contMR, "merged" = mergedTable))
+}
