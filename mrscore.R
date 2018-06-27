d7f7d87de4cf84e092d7dff37cc12ece69c529c8
Espen Riskedal <espen@cutehacks.com>
2018-06-27 21:44:27 +0200

    feat: map continous risk score to hazard ratio

--- mrscore.R
+++ mrscore.R
@@ -5,10 +5,15 @@
 
 
 MRscoreCpgSites = read.csv("cpgsites_multipliers_cutoffs.csv", strip.white = TRUE, blank.lines.skip = TRUE, comment.char = "#")
+HRMapping =  read.csv("continousrisk_to_hr_mapping.csv", strip.white = TRUE, blank.lines.skip = TRUE, comment.char = "#")
+EstherSpline = smooth.spline(HRMapping$Risk, HRMapping$HREsther, df=7)
+KoraSpline = smooth.spline(HRMapping$Risk, HRMapping$HRKora, df=7)
 
 calculateMRscore = function(geoMethylationTable) {
   mergedTable = merge(MRscoreCpgSites, geoMethylationTable, by.x = "CpGsites", by.y = "ID_REF")
   noncontMR = sum(mergedTable$VALUE <= mergedTable$Lower) + sum(mergedTable$VALUE >= mergedTable$Higher)
   contMR = sum(mergedTable$VALUE * mergedTable$Multipliers)
-  return(list("MRscore" = noncontMR, "contMRscore" = contMR, "merged" = mergedTable))
+  contHREster = predict(EstherSpline, contMR)$y
+  contHRKora = predict(KoraSpline, contMR)$y
+  return(list("MRscore" = noncontMR, "contMRscore" = contMR, "contHREsther" = contHREster, "contHKora" = contHRKora, "merged" = mergedTable))
 }
