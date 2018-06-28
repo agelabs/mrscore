83282dbbf565fcebe10abb11fb3016a1573d34c8
Espen Riskedal <espen@cutehacks.com>
2018-06-28 08:45:03 +0200

    fix: forgot to add RiskMapping
    - also, renamed HRMapping to ContRiskMapping

--- mrscore.R
+++ mrscore.R
@@ -3,11 +3,11 @@
 # source("mrscore.R")
 # calculateMRscore(Table(getGEO("GSM1886935")))
 
-
 MRscoreCpgSites = read.csv("cpgsites_multipliers_cutoffs.csv", strip.white = TRUE, blank.lines.skip = TRUE, comment.char = "#")
-HRMapping =  read.csv("continousrisk_to_hr_mapping.csv", strip.white = TRUE, blank.lines.skip = TRUE, comment.char = "#")
-EstherSpline = smooth.spline(HRMapping$Risk, HRMapping$HREsther, df=7)
-KoraSpline = smooth.spline(HRMapping$Risk, HRMapping$HRKora, df=7)
+RiskMapping =  read.csv("risk_to_hr_mapping.csv", strip.white = TRUE, blank.lines.skip = TRUE, comment.char = "#")
+ContRiskMapping = read.csv("continousrisk_to_hr_mapping.csv", strip.white = TRUE, blank.lines.skip = TRUE, comment.char = "#")
+EstherSpline = smooth.spline(ContRiskMapping$Risk, ContRiskMapping$HREsther, df=7)
+KoraSpline = smooth.spline(ContRiskMapping$Risk, ContRiskMapping$HRKora, df=7)
 
 calculateMRscore = function(geoMethylationTable) {
   mergedTable = merge(MRscoreCpgSites, geoMethylationTable, by.x = "CpGsites", by.y = "ID_REF")
